e837ba64f4771cbda51e8527a1a67eb528c8d698
Паша <markovpavel32@gmail.com>
2018-10-06 22:32:36 +0300

    v1.5.1

--- src/js/createData.js
+++ src/js/createData.js
@@ -1,9 +1,5 @@
 export default function createData(values) {
-    let data = [];
-    function makeArr() {
-        data = values.split('').map(x => +x);
-    }
-    makeArr();
     
-    return data;    
+    return values.split('').map(x => +x);
+
 }
--- src/js/render.js
+++ src/js/render.js
@@ -1,50 +1,41 @@
-import {dispatcher as signal} from "./dispatcher.js";
-import {bubbleSort} from './index.js';
 const Render = {
     
     parent: document.getElementsByClassName('parent')[0],
 
     render(arr){
-        let bars = bubbleSort.arrayOfBars;
-        let indexes = bubbleSort.arrayOfIndexes;
-        let counter = bubbleSort.counter;
-        let count = counter.get() - 1;
-        let lay = bubbleSort.lay;
-        let layLength = lay.length;
-        let chIndex = bubbleSort.changeIndex;
 
-        if(signal.flag === 'first initialization') {
-            while(this.parent.firstChild){
-                this.parent.removeChild(this.parent.firstChild);
-            }
-
-            let div = [];
+        while(this.parent.firstChild){
+            this.parent.removeChild(this.parent.firstChild);
+        }
 
             for(let i = 0; i < arr.length; i++){
-                div[i] = document.createElement('div');
-                this.parent.appendChild(div[i]);
-                div[i].classList.add('bar');
+                let div = document.createElement('div');
+                this.parent.appendChild(div);
+                div.classList.add('bar');
                 let h = arr[i] * 20;
-                div[i].style.height = `${2 * h}px`;
-                div[i].style.left = `${i * 25}px`;
-                div[i].innerHTML = `${arr[i]}`;
+                div.style.cssText = `background-color: #ff5a00;\
+                height: ${2 * h}px;\
+                width: 20px;\
+                left: ${i * 25}px;\
+                `
+                div.innerHTML = `${arr[i]}`;
             }
+    },
 
+    stepForward(obj){
+        let bar = document.getElementsByClassName('bar');
+        for(let i = 0; i < obj.data.length; i++){
+            bar[obj.indexes[i]].style.left = `${i * 25}px`;
+        }
+    },
 
-        } else if(signal.flag === 'step forward'){
-            let f = bars[indexes[count]].style.left;
-            bars[indexes[count]].style.left = bars[indexes[count+1]].style.left;
-            bars[indexes[count+1]].style.left = f;
-             
-
-        
-        } else if(signal.flag === 'step backward'){
-            console.log(chIndex);
-            let c = bars[lay[layLength - 1][chIndex]].style.left;
-            bars[lay[layLength - 1][chIndex]].style.left = bars[lay[layLength - 1][chIndex+1]].style.left;
-            bars[lay[layLength - 1][chIndex+1]].style.left = c;
+    stepBackward(obj){
+        let bar = document.getElementsByClassName('bar');
+        for(let i = 0; i < obj.data.length; i++){
+            bar[obj.lay[obj.lay.length-1][i]].style.left = `${i * 25}px`;
         }
     }
 }
+
 
 export default Render;
