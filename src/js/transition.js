1cd30661d65b619e491c35ead26efeb645f4d389
Паша <markovpavel32@gmail.com>
2018-10-06 22:33:56 +0300

    v1.5.2

--- src/js/transition.js
+++
@@ -1,8 +0,0 @@
-const transition = () => {
-                let c = arrayOfBars[arrayOfIndexes[j]].style.left;
-                arrayOfBars[arrayOfIndexes[j]].style.left = arrayOfBars[arrayOfIndexes[j+1]].style.left;
-                arrayOfBars[arrayOfIndexes[j+1]].style.left = c;
-                let d = arrayOfIndexes[j];
-                arrayOfIndexes[j] = arrayOfIndexes[j+1];
-                arrayOfIndexes[j + 1] = d;
-}
